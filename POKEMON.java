177d3a300b8267b8064e4c2959f8b2d061bd8c51
ahmedalturkaitC88 <70819173+ahmedalturkaitC88@users.noreply.github.com>
2020-10-03 19:35:56 +0300

    AAAA

---
+++ POKEMON.java
@@ -0,0 +1,52 @@
+package com.example.pokemon;
+
+public class POKEMON {
+
+
+    String name;
+    int image;
+    int attack;
+    int defence;
+    int total;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
